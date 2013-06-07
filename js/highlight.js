11e2387708a01b754e9856cacfb215f54e7ec71f
r.grannell1 <r.grannell1@nuigalway.ie>
2013-06-07 17:59:13 +0100

    refactored code into one global variable

--- js/highlight.js
+++ js/highlight.js
@@ -3,12 +3,11 @@
 "use strict";
 
 HIGHLIGHT = ( function () {
+	// the global variable for the entire highlight app
+
 	return {
 		LEVEL: 0,
 		StateMachine: function (states) {
-			/* constructor for state machine; takes an object
-
-			*/
 
 			this.states = states;
 
@@ -27,17 +26,17 @@ HIGHLIGHT = ( function () {
 
 			this.consume_token = function (token) {
 
-				var change_bracket_level = function (source, target) {
+				var set_LEVEL = function (source, target) {
 					// change r output depth based on transition
 
-					var parens_opened = source === 'normal' && target === 'open_delim' || 
+					var delimiter_opened = source === 'normal' && target === 'open_delim' || 
 						source === 'open_delim' && target === 'open_delim';
 					
-					var parens_closed = source === 'close_delim' && target === 'normal' ||
+					var delimiter_closed = source === 'close_delim' && target === 'normal' ||
 						source === 'close_delim' && target === 'close_delim';
 
-					if (parens_opened) HIGHLIGHT.LEVEL = HIGHLIGHT.LEVEL + 1;
-					if (parens_closed) HIGHLIGHT.LEVEL = HIGHLIGHT.LEVEL - 1;
+					if (delimiter_opened) HIGHLIGHT.LEVEL = HIGHLIGHT.LEVEL + 1;
+					if (delimiter_closed) HIGHLIGHT.LEVEL = HIGHLIGHT.LEVEL - 1;
 
 					r_output_rules = output_rules(HIGHLIGHT.LEVEL); ///
 				}
@@ -60,186 +59,189 @@ HIGHLIGHT = ( function () {
 				this.states[old_state].active = false;
 				this.states[new_state].active = true;
 			
-				// alters global; HIGHLIGHT.level
-				change_bracket_level(old_state, new_state);
+				// alters global; HIGHLIGHT.LEVEL
+				set_LEVEL(old_state, new_state);
 
 				return this.output_token(old_state, new_state, token);
 
 			}	
+		},
+		r_state_transitions: ( function () {
+			// returns an object which contains objects - one for each possible state -
+			// which contain an active field (is this the state we're currently on?) and 
+			// edges: tokens that trigger a state change.
+
+			var normal_and_delim_edges = ( function () {
+				// characters that trigger a state transition - and the states they cause
+				// the move to - that are used by the normal state and the delimiter states
+
+				return {
+					"'": 'str_single',
+					'"': 'str_double',
+					'#': 'comment',
+					
+					'(': 'open_delim',
+					'[': 'open_delim',
+					'{': 'open_delim',
+					
+					'}': 'close_delim',
+					']': 'close_delim',
+					')': 'close_delim',
+					
+					'*nomatch*': 'normal'
+				}
+			} )()
+
+			return {
+				'str_single': {
+					'active': false,
+					'edges': {
+						"'": 'normal',
+						'*nomatch*': 'str_single'
+					}
+				},
+				'str_double': {
+					'active': false,
+					'edges': {
+						'"': 'normal',
+						'*nomatch*': 'str_double'
+					} 
+				},
+				'normal': {
+					'active': true,
+					'edges': normal_and_delim_edges()
+				},
+				
+				'open_delim': {
+					'active': false,
+					'edges': normal_and_delim_edges()
+				},
+				'close_delim': {
+					'active': false,
+					'edges': normal_and_delim_edges()
+				},
+				'comment': {
+					'active': false,
+					'edges': {
+						'\n': 'normal',
+						"'": 'roxygen',
+						'*nomatch*': 'comment'
+					}
+				},
+				'roxygen': {
+					'active': false,
+					'edges': {
+						'\n': 'normal',
+						'*nomatch*': 'roxygen'
+					}
+				}
+			}
+		} )(),
+		output_rules: function(level) {
+			// generates an object describing state-state transitions for 
+			// the R grammar highlighter; each edge is associated with some html output.
+			// The output is dependent on level,
+			// a global variable denoting how many levels nested the state machine
+			// parsing this grammar currently is.
+		 
+
+			var span_open = function (class) {
+				return '<span class="' + class + '">'
+			}
+			var span_close = function () {
+				return '</span>'
+			}
+			var span = function (class, content) {
+				return '<span class="' + class + '">' + content + '</span>'
+			}
+
+			var open_delim_output = function (level) {
+
+				return {
+					'(': span('lev' + level, '('),
+					'[': span('lev' + level, '['),
+					'{': span('lev' + level, '{')
+				}
+			}
+
+			var close_delim_output = function (level) {
+
+				return {
+					')': span('lev' + level, ')'),
+					']': span('lev' + level, ']'),
+					'}': span('lev' + level, '}')
+				}
+			}
+
+			var html_normal_and_delim = function (level) {
+				// the html output associated with certain tokens
+				// when in normal or delimiter states
+
+				return {
+					'str_single': {
+						"'": span_open("sstring") + "'"
+					},
+					'str_double': {
+						'"': span_open("dstring") + '"'
+					},
+					'normal': {
+						'$': span('dollar', '$'),
+						',': span('comma lev' + level, ','),
+						'*nomatch*': '*token*'
+					},
+					'comment': {
+						'#': span_open('comment') + '#'
+					},
+					'open_delim': open_delim_output(level),
+					'close_delim': close_delim_output(level)
+				}
+			}
+
+			return {
+				'str_single': {
+					'str_single': {
+						'"': span("ssdouble", '"'),
+						'*nomatch*': '*token*'
+					},
+					'normal': {
+						"'": "'" + span_close() 
+					}
+				},
+				'str_double': {
+					'str_double': {
+						"'": span("dssingle", "'"),
+						'*nomatch*': '*token*'
+					},
+					'normal': {
+						'"': '"' + span_close()
+					}
+				},
+				'normal': html_normal_and_delim(level),
+				'open_delim': html_normal_and_delim(level),
+				'close_delim': html_normal_and_delim(level),
+				'comment': {
+					'normal': {
+						'\n': '\n' + span_close()
+					},
+					'comment': {
+						'*nomatch*': '*token*'
+					},
+					'roxygen': {
+						"'": "'" + span_close() + span_open('roxygen')
+					}
+				},
+				'roxygen': {
+					'normal': {
+						'\n': '\n' + span_close()
+					},
+					'roxygen': {
+						'@': span_open('at', '@')
+					}
+				}
+			}
 		}
-
-
 	}
 } )();
 
-var span_open = function (class) {
-	return '<span class="' + class + '">'
-}
-var span_close = function () {
-	return '</span>'
-}
-var span = function (class, content) {
-	return '<span class="' + class + '">' + content + '</span>'
-}
 
-var norm_or_paren_rules = function () {
-	
-	return {
-		"'": 'str_single',
-		'"': 'str_double',
-		'#': 'comment',
-		
-		'(': 'open_delim',
-		'[': 'open_delim',
-		'{': 'open_delim',
-		
-		'}': 'close_delim',
-		']': 'close_delim',
-		')': 'close_delim',
-		
-		'*nomatch*': 'normal'
-	}
-} 
 
-open_delim_output = function (level) {
-	return {
-		'(': span('lev' + level, '('),
-		'[': span('lev' + level, '['),
-		'{': span('lev' + level, '{')
-	}
-}
-
-close_delim_output = function (level) {
-	return {
-		')': span('lev' + level, ')'),
-		']': span('lev' + level, ']'),
-		'}': span('lev' + level, '}')
-	}
-}
-
-r_state_transitions = ( function () {
-	// returns an object which contains objects - one for each possible state -
-	// which contain an active field (is this the state we're currently on?) and 
-	// edges: tokens that trigger a state change.
-
-	return {
-		'str_single': {
-			'active': false,
-			'edges': {
-				"'": 'normal',
-				'*nomatch*': 'str_single'
-			}
-		},
-		'str_double': {
-			'active': false,
-			'edges': {
-				'"': 'normal',
-				'*nomatch*': 'str_double'
-			} 
-		},
-		'normal': {
-			'active': true,
-			'edges': norm_or_paren_rules()
-		},
-		
-		'open_delim': {
-			'active': false,
-			'edges': norm_or_paren_rules()
-		},
-		'close_delim': {
-			'active': false,
-			'edges': norm_or_paren_rules()
-		},
-		'comment': {
-			'active': false,
-			'edges': {
-				'\n': 'normal',
-				"'": 'roxygen',
-				'*nomatch*': 'comment'
-			}
-		},
-		'roxygen': {
-			'active': false,
-			'edges': {
-				'\n': 'normal',
-				'*nomatch*': 'roxygen'
-			}
-		}
-	}
-} )()
-
-output_rules = function(level) {
-	// generates an object describing state-state transitions for 
-	// the R grammar highlighter; each edge is associated with some html output.
-	// The output is dependent on level,
-	// a global variable denoting how many levels nested the state machine
-	// parsing this grammar currently is.
- 
-	// a factored out bit of grammar for output rules;
-	// output rules in normal or paren states
-	
-	var html_output_normal = function (val) {
-
-		return {
-			'str_single': {
-				"'": span_open("sstring") + "'"
-			},
-			'str_double': {
-				'"': span_open("dstring") + '"'
-			},
-			'normal': {
-				'$': span('dollar', '$'),
-				',': span('comma lev' + val, ','),
-				'*nomatch*': '*token*'
-			},
-			'comment': {
-				'#': span_open('comment') + '#'
-			},
-			'open_delim': open_delim_output(val),
-			'close_delim': close_delim_output(val)
-		}
-	}
-
-	return {
-		'str_single': {
-			'str_single': {
-				'"': span("ssdouble", '"'),
-				'*nomatch*': '*token*'
-			},
-			'normal': {
-				"'": "'" + span_close() 
-			}
-		},
-		'str_double': {
-			'str_double': {
-				"'": span("dssingle", "'"),
-				'*nomatch*': '*token*'
-			},
-			'normal': {
-				'"': '"' + span_close()
-			}
-		},
-		'normal': html_output_normal(level),
-		'open_delim': html_output_normal(level),
-		'close_delim': html_output_normal(level),
-		'comment': {
-			'normal': {
-				'\n': '\n' + span_close()
-			},
-			'comment': {
-				'*nomatch*': '*token*'
-			},
-			'roxygen': {
-				"'": "'" + span_close() + span_open('roxygen')
-			}
-		},
-		'roxygen': {
-			'normal': {
-				'\n': '\n' + span_close()
-			},
-			'roxygen': {
-				'@': span_open('at', '@')
-			}
-		}
-	}
-}
