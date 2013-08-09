56cecc9bd9961af65456b650ad7e58dead50122d
rgrannell1 <r.grannell1@nuigalway.ie>
2013-08-09 23:22:25 +0100

    improved clarity of code, moved from using the new keyword

--- js/highlight.js
+++ js/highlight.js
@@ -1,134 +1,146 @@
 
-
 (function () {
-	"use strict";
+	"use strict"
 } )()
 
-var HIGHLIGHT = ( function () {
-	// global object for highlight.js (great name, huh?).
-	// released under the GLP-3, copyright Ryan Grannell.
+highlight = ( function () {
+	/* global object for highlight.js (great name, huh?).
+	  released under the GLP-3, copyright Ryan Grannell. */
 
 	return {
 		StateMachine: function (states, outputs) {
 
-			var fsm_state = {
-				level: 0,
-				state_transitions: states,
-				output_rules: outputs( this.level )
+			var that = {
+				depth: 0,
+				transitions: states,
+				output_rules: outputs( this.depth )				
 			}
 
-			this.consume_token = function (token) {
-				// takes a single token, updates internal state if
-				// the token caused a state -> state transition. returns a
-				// pretty html string to the user.
+			that.consume_token = function (token) {
+				/* takes a single token, updates internal state if
+				 the token caused a state -> state transition. returns a
+				 pretty html string to the user. */
 
 				var html_output = function (source, target, token) {
-					// takes a source state, target state and a token that
-					// triggered the transition. returns a html string that
-					// styles the input token
+					/* takes a source state, target state and a token that
+					 triggered the transition. returns a html string that
+					 styles the input token. */
 
-					var state_rules = fsm_state.output_rules[source][target];
-					var html_string = token;
+					var state_rules = that.output_rules[source][target]
+					var html_string = token
 
 					for (var candidate in state_rules) {
+						if (!state_rules.hasOwnProperty(candidate)) {
+							continue
+						}
 						if (candidate !== '*nomatch*' && candidate === token) {
-							html_string = state_rules[candidate];
+							html_string = state_rules[candidate]
 						}			
 					}	
-					return html_string;
+					return html_string
 				}
 
-				var change_level_state = function (source, target, level) {
+				var change_depth_state = function (source, target, depth) {
 					// change r output depth based on transition
 
 					var delimiter_opened = 
 						(source === 'normal' && 
 							target === 'open_delim') || 
 						(source === 'open_delim' &&
-							target === 'open_delim');
+							target === 'open_delim')
 					
 					var delimiter_closed = 
 						(source === 'close_delim' &&
 							target === 'normal') ||
 						(source === 'close_delim' &&
-							target === 'close_delim');
+							target === 'close_delim')
 
 					if (delimiter_opened) {
-						level += 1;
+						depth += 1
 					} else if (delimiter_closed) {
-						level -= 1;
+						depth -= 1
 					}
 
-					fsm_state.level = level;
-					fsm_state.output_rules = HIGHLIGHT.output_rules(level);
+					that.depth = depth
+					that.output_rules = highlight.output_rules(depth)
 
 					return {
-						"level": level, 
-						"output_rules": HIGHLIGHT.output_rules(level) 
+						"depth": depth, 
+						"output_rules": highlight.output_rules(depth) 
 					}
 				}
 
-				token = token + "";
+				token = token + ""
 				
-				for (var st in fsm_state.state_transitions) {
-					if (fsm_state.state_transitions[st].active) {
-						var active = fsm_state.state_transitions[st];
-						var old_state = st;
+				for (var transition in that.transitions) {
+					if (!that.transitions.hasOwnProperty(transition)) {
+						continue
+					}
+
+					if (that.transitions[transition].active) {
+						var active = that.transitions[transition]
+						var old_state = transition
 					}
 				}
 
-				var new_state = active.edges['*nomatch*'];
-				for (var ed in active.edges) {
-					if (token === ed) new_state = active.edges[ed];
+				var new_state = active.edges['*nomatch*']
+				for (var edge in active.edges) {
+					if (!active.edges.hasOwnProperty(edge)) {
+						continue
+					}					
+					if (token === edge) {
+						new_state = active.edges[edge]
+					}
 				}
 
-				fsm_state.state_transitions[old_state].active = false;
-				fsm_state.state_transitions[new_state].active = true;
+				that.transitions[old_state].active = false
+				that.transitions[new_state].active = true
 			
-				change_level_state(old_state, new_state, fsm_state.level);
+				change_depth_state(old_state, new_state, that.depth)
 
-				return html_output(old_state, new_state, token);
+				return html_output(old_state, new_state, token)
 
 			}
+			return that
 		},
 		highlight_text: function (text) {
-			// given (presumably legal) R code as a single string, 
-			// return a string of higlighted R code
+			/* given (presumably legal) R code as a single string, 
+			 return a string of higlighted R code */
 
-			var highlighted_code = '';
-			var r_state_machine = new HIGHLIGHT.StateMachine(
-				HIGHLIGHT.r_state_transitions,
-				HIGHLIGHT.output_rules);
+			var highlighted_code = ''
+			var r_state_machine = highlight.StateMachine(
+				highlight.r_transitions,
+				highlight.output_rules)
 
-			for (var i = 0; i < text.length; i++) {
+			for (var ith = 0; ith < text.length; ith++) {
 			
-				var token = text.substring(i, i+1);
+				var token = text.substring(ith, ith+1)
 				highlighted_code = 
-					highlighted_code + r_state_machine.consume_token(token);
+					highlighted_code + r_state_machine.consume_token(token)
 			
 			}
 
 			return highlighted_code
 		},
 		highlight_r_code: function () {
-			// alter all class = "r" tags in a html document,
-			// returning code that can be targeted with css
+			/* alter all class = "r" tags in a html document,
+			 returning code that can be targeted with css. */
 
 			$('.r').replaceWith( function (index, content) {
 				return '<code class = "r">' + 
-					HIGHLIGHT.highlight_text($(this).text()) + 
-				'</code>';
-			} );
+					highlight.highlight_text($(this).text()) + 
+				'</code>'
+			} )
 
 		},
-		r_state_transitions: ( function () {
-			// returns an object which contains objects - one for each possible state -
-			// which contain an active field (is this the state we're currently on?) and 
-			// edges: tokens that trigger a state change.
+		r_transitions: ( function () {
+			/* returns an object which contains objects - one for each possible state -
+			 which contain an active field (is this the state we're currently on?) and 
+			 edges: tokens that trigger a state change. */
 
 			var normal_and_delim_edges = ( function () {
-				// characters that trigger a state transition - and the states they cause
-				// the move to - that are used by the normal state and the delimiter states
+				/* characters that trigger a state transition - and the states they cause
+				 the move to - that are used by the normal state and the delimiter states. */
 
 				return {
 					"'": 'str_single',
@@ -192,12 +204,12 @@ var HIGHLIGHT = ( function () {
 				}
 			}
 		} )(),
-		output_rules: function(level) {
-			// generates an object describing state-state transitions for 
-			// the R grammar highlighter; each edge is associated with some html output.
-			// The output is dependent on level,
-			// a global variable denoting how many levels nested the state machine
-			// parsing this grammar currently is.
+		output_rules: function(depth) {
+			/* generates an object describing state-state transitions for 
+			 the R grammar highlighter each edge is associated with some html output.
+			 The output is dependent on depth,
+			 a global variable denoting how many depths nested the state machine
+			 parsing this grammar currently is. */
 
 			var span_open = function (css_class) {
 				return '<span class="' + css_class + '">'
@@ -209,27 +221,27 @@ var HIGHLIGHT = ( function () {
 				return '<span class="' + css_class + '">' + content + '</span>'
 			}
 
-			var open_delim_output = function (level) {
+			var open_delim_output = function (depth) {
 
 				return {
-					'(': span('lev' + level, '('),
-					'[': span('lev' + level, '['),
-					'{': span('lev' + level, '{')
+					'(': span('lev' + depth, '('),
+					'[': span('lev' + depth, '['),
+					'{': span('lev' + depth, '{')
 				}
 			}
 
-			var close_delim_output = function (level) {
+			var close_delim_output = function (depth) {
 
 				return {
-					')': span('lev' + level, ')'),
-					']': span('lev' + level, ']'),
-					'}': span('lev' + level, '}')
+					')': span('lev' + depth, ')'),
+					']': span('lev' + depth, ']'),
+					'}': span('lev' + depth, '}')
 				}
 			}
 
-			var html_normal_and_delim = function (level) {
-				// the html output associated with certain tokens
-				// when in normal or delimiter states
+			var html_code_state = function (depth) {
+				/* the html output associated with certain tokens
+				 when in normal or delimiter states. */
 
 				return {
 					'str_single': {
@@ -240,14 +252,14 @@ var HIGHLIGHT = ( function () {
 					},
 					'normal': {
 						'$': span('dollar', '$'),
-						',': span('comma lev' + level, ','),
+						',': span('comma lev' + depth, ','),
 						'*nomatch*': '*token*'
 					},
 					'comment': {
 						'#': span_open('comment') + '#'
 					},
-					'open_delim': open_delim_output(level),
-					'close_delim': close_delim_output(level)
+					'open_delim': open_delim_output(depth),
+					'close_delim': close_delim_output(depth)
 				}
 			}
 
@@ -270,9 +282,9 @@ var HIGHLIGHT = ( function () {
 						'"': '"' + span_close()
 					}
 				},
-				'normal': html_normal_and_delim(level),
-				'open_delim': html_normal_and_delim(level),
-				'close_delim': html_normal_and_delim(level),
+				'normal': html_code_state(depth),
+				'open_delim': html_code_state(depth),
+				'close_delim': html_code_state(depth),
 				'comment': {
 					'normal': {
 						'\n': '\n' + span_close()
@@ -295,7 +307,7 @@ var HIGHLIGHT = ( function () {
 			}
 		}
 	}
-} )();
+} )()
 
 
 
